8501d569c4a436e96a98c3db4755ed25c1f82584
Siegfried Horschig <siegfried.horschig@student.hpi.de>
2018-02-07 17:55:11 +0100

    Add util module

---
+++ util.py
@@ -0,0 +1,9 @@
+import string
+
+
+def format_filename(s):
+    """Take a string and return a valid filename constructed from it."""
+    valid_chars = "-_.() %s%s" % (string.ascii_letters, string.digits)
+    filename = ''.join(c for c in s if c in valid_chars)
+    filename = filename.replace(' ', '_')  # I don't like spaces in filenames.
+    return filename
